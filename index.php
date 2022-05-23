720b15b7b56d5db40460584da5f28be00758b606
abel-dejene <abelhailemichael30@gmail.com>
2022-05-23 21:18:42 +0300

    adding the form to the file

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VotingApp</title>
+
+    <!-- Bootstrap css link-->
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body class="bg-dark">
+    <h1> heading tag is here </h1>
+    <form action="get">
+        <input type="text" name="gete production" id="">
+        <input type="email" name="" id="">
+        <input type="password" name="" id="">
+    
+    </form>
+    
+</body>
+</html>
